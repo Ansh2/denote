595d8e8f9bac117cf15379e367377ae57b82546f
voomp <bat7gamer7@gmail.com>
2022-10-22 23:55:36 -0400

    change textarea

--- src/notes.tsx
+++ src/notes.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 import Content from './components/Content';
 import noteData from "./data/notes.json";
-import regeneratorRuntime from "regenerator-runtime";
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+// import regeneratorRuntime from "regenerator-runtime";
+// import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 import icon from './arrow-back-circle-outline.svg';
 import { Link, useParams } from 'react-router-dom';
 
@@ -10,21 +10,21 @@ const ClassNotes = () => {
 	const [currNotes, setNotes] = useState({});
 	const params = useParams();
 	const noteClass = params.class;
-	const {
-		transcript,
-		listening,
-		resetTranscript,
-		browserSupportsSpeechRecognition
-	} = useSpeechRecognition();
-	if (!browserSupportsSpeechRecognition) {
-		return (<span>Browser doesn&apos;t support speech recognition.</span>);
-	}
+	// const {
+	// 	transcript,
+	// 	listening,
+	// 	resetTranscript,
+	// 	browserSupportsSpeechRecognition
+	// } = useSpeechRecognition();
+	// if (!browserSupportsSpeechRecognition) {
+	// 	return (<span>Browser doesn&apos;t support speech recognition.</span>);
+	// }
 	
-	// useEffect(() => {
-	// 	const val = localStorage.getItem('notes');
-	// 	setNotes(!!val ? JSON.parse(val) : noteData);
-	// 	if (!val) localStorage.setItem('notes', JSON.stringify(noteData));
-	// });
+	useEffect(() => {
+		const val = localStorage.getItem('notes');
+		setNotes(!!val ? JSON.parse(val) : noteData);
+		if (!val) localStorage.setItem('notes', JSON.stringify(noteData));
+	}, []);
 
 	return (
 		<Content>
@@ -36,7 +36,7 @@ const ClassNotes = () => {
 							<div className="text-[#F43F5E] text-[36px] font-medium">Connections</div>
 						</div>
 					</Link>	
-					<textarea className="min-h-[500px] w-full bg-[#262626] text-[#E5E5E5]" />
+					<textarea className="min-h-[500px] w-full bg-[#262626] text-[20px] text-[#E5E5E5]" />
 				</div>
 
 				<div className="px-10 w-full">
@@ -49,14 +49,14 @@ const ClassNotes = () => {
 						<button onClick={() => SpeechRecognition.stopListening()} className="px-5 py-2 bg-brand rounded flex justify-center items-center">Stop recording</button>
 						<button onClick={resetTranscript}>Reset</button>
 					</div> */}
-					<textarea className="min-h-[500px] w-full bg-[#262626] text-[#E5E5E5] p-5 z-10" />
+					<textarea className="min-h-[500px] w-full bg-[#262626] text-[#E5E5E5] text-[20px] pt-5" />
 				</div>
 			</div>
 			<div>
 				<div className="text-[36px] text-brand font-medium">
 					Summary
 				</div>
-				<textarea className="min-h-[200px] w-full bg-[#262626] text-[#E5E5E5] p-5 z-10" />
+				<textarea className="min-h-[200px] w-full bg-[#262626] text-[#E5E5E5] text-[20px]" />
 			</div>
 			<button className="px-5 py-2 bg-brand rounded flex justify-center items-center my-[50px]">Save</button>
 
