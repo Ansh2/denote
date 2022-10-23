d76d49853bcd8c4aa9e06c655c9a69664700b3ad
voomp <bat7gamer7@gmail.com>
2022-10-23 06:21:24 -0400

    fixes

--- src/notes.tsx
+++ src/notes.tsx
@@ -1,30 +1,46 @@
 import { useEffect, useState } from 'react';
 import Content from './components/Content';
-import noteData from "./data/notes.json";
-// import regeneratorRuntime from "regenerator-runtime";
-// import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 import icon from './arrow-back-circle-outline.svg';
 import { Link, useParams } from 'react-router-dom';
+import noteData from './data/notes.json';
 
 const ClassNotes = () => {
-	const [currNotes, setNotes] = useState({});
+	const [currNotes, setNotes] = useState<any>({});
 	const params = useParams();
 	const noteClass = params.class;
-	// const {
-	// 	transcript,
-	// 	listening,
-	// 	resetTranscript,
-	// 	browserSupportsSpeechRecognition
-	// } = useSpeechRecognition();
-	// if (!browserSupportsSpeechRecognition) {
-	// 	return (<span>Browser doesn&apos;t support speech recognition.</span>);
-	// }
+
+	const [notesValue, setNotesVal] = useState('');
+	const [connValue, setConnections] = useState('');
+	const [summaryValue, setSummary] = useState('');
 	
 	useEffect(() => {
 		const val = localStorage.getItem('notes');
-		setNotes(!!val ? JSON.parse(val) : noteData);
+		setNotes((!!val) ? JSON.parse(val) : noteData);
 		if (!val) localStorage.setItem('notes', JSON.stringify(noteData));
-	}, []);
+	}, [noteClass]);
+
+	const saveChanges = () => {
+		if(!noteClass) return;
+		const newNotes = {...currNotes};
+		newNotes[noteClass].notes = notesValue;
+		newNotes[noteClass].connections = connValue;
+		newNotes[noteClass].summary = summaryValue;
+		setNotes(newNotes);
+	}
+
+	useEffect(() => {
+		if(Object.keys(currNotes).length === 0) return;
+		localStorage.setItem('notes', JSON.stringify(currNotes));
+		console.log(currNotes);
+		if(!noteClass) return;
+		const tempData: any = {...currNotes};
+		console.log(tempData[noteClass]);
+		setNotesVal(tempData[noteClass].notes);
+		setConnections(tempData[noteClass].connections);
+		setSummary(tempData[noteClass].summary);
+	}, [currNotes])
+
+	if(!noteClass) return <></>;
 
 	return (
 		<Content>
@@ -36,29 +52,23 @@ const ClassNotes = () => {
 							<div className="text-[#F43F5E] text-[36px] font-medium">Connections</div>
 						</div>
 					</Link>	
-					<textarea className="min-h-[500px] w-full bg-[#262626] text-[20px] text-[#E5E5E5]" />
+					<textarea className="min-h-[500px] w-full bg-[#262626] text-[20px] text-[#E5E5E5]" value={connValue} onChange={(e) => setConnections(e.target.value)} />
 				</div>
 
 				<div className="px-10 w-full">
 					<div className="text-[36px] text-brand font-medium">
 						{noteClass} Notes
 					</div>
-					{/* <div className="flex space-x-5 my-5 items-center">
-						<p>Microphone: {listening ? 'on' : 'off'}</p>
-						<button onClick={() => SpeechRecognition.startListening()} className="px-5 py-2 bg-brand rounded flex justify-center items-center">Start recording</button>
-						<button onClick={() => SpeechRecognition.stopListening()} className="px-5 py-2 bg-brand rounded flex justify-center items-center">Stop recording</button>
-						<button onClick={resetTranscript}>Reset</button>
-					</div> */}
-					<textarea className="min-h-[500px] w-full bg-[#262626] text-[#E5E5E5] text-[20px] pt-5" />
+					<textarea className="min-h-[500px] w-full bg-[#262626] text-[#E5E5E5] text-[20px] pt-5" value={notesValue} onChange={(e) => setNotesVal(e.target.value)} />
 				</div>
 			</div>
 			<div>
 				<div className="text-[36px] text-brand font-medium">
 					Summary
 				</div>
-				<textarea className="min-h-[200px] w-full bg-[#262626] text-[#E5E5E5] text-[20px]" />
+				<textarea className="min-h-[200px] w-full bg-[#262626] text-[#E5E5E5] text-[20px]" value={summaryValue} onChange={(e) => setSummary(e.target.value)} />
 			</div>
-			<button className="px-5 py-2 bg-brand rounded flex justify-center items-center my-[50px]">Save</button>
+			<button className="px-5 py-2 bg-brand rounded flex justify-center items-center my-[50px]" onClick={saveChanges} >Save</button>
 
 		</Content>
 	)
